7059c4047b1014ea6ec8994ec30edede2f03270e
Dhruv Takwal <dhruv@rapidinnovation.dev>
2021-12-06 14:34:09 +0530

    Update sorting and pagination in explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -26,7 +26,7 @@ import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
-import { red } from "@mui/material/colors";
+import { red, blue, green } from "@mui/material/colors";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
@@ -37,6 +37,11 @@ import ActiveIcon from "@mui/icons-material/AccessTimeFilled";
 import CancelIcon from "@mui/icons-material/Cancel";
 import CompleteIcon from "@mui/icons-material/CheckCircle";
 import RewardIcon from "@mui/icons-material/Stars";
+import DepositIcon from "@mui/icons-material/AccountBalance";
+import FeeIcon from "@mui/icons-material/Receipt";
+import CountIcon from "@mui/icons-material/SupervisorAccount";
+import LeftIcon from "@mui/icons-material/GroupAdd";
+import Skeleton from "@mui/material/Skeleton";
 
 import client from "../graphql/client";
 import { FETCH_ALL_POOLS } from "../graphql/queries/fetchPools";
@@ -54,28 +59,41 @@ const Explore = () => {
   const [loading, setLoading] = useState(false);
   const [pools, setPools] = useState([]);
   const [filteredPools, setFilteredPools] = useState([]);
+  const [activeSort, setActiveSort] = useState("id-asc");
+  const [activePage, setActivePage] = useState(1);
 
   const [orderBy, setOrderBy] = useState("startTime");
   const [orderDirection, setOrderDirection] = useState("asc");
-  const [first, setFirst] = useState(6);
+  const [first, setFirst] = useState(4);
   const [skip, setSkip] = useState(0);
 
   const handleTab = (tab) => {
+    setActivePage(1);
+    setSkip(0);
     setActiveTab(tab);
+
+    const [orderBy, orderDirection] = activeSort.split("-");
+
     setFilteredPools(
-      pools.filter(({ status }) => {
-        if (tab === 0) {
-          return status !== "";
-        } else if (tab === 1) {
-          return status === "ACTIVE";
-        } else if (tab === 2) {
-          return status === "COMPLETE";
-        } else if (tab === 3) {
-          return status === "CANCELLED";
-        } else {
-          return status !== "";
-        }
-      })
+      pools
+        .filter(({ status }) => {
+          if (tab === 0) {
+            return status !== "";
+          } else if (tab === 1) {
+            return status === "ACTIVE";
+          } else if (tab === 2) {
+            return status === "COMPLETE";
+          } else if (tab === 3) {
+            return status === "CANCELLED";
+          } else {
+            return status !== "";
+          }
+        })
+        .sort((x, y) =>
+          orderDirection === "asc"
+            ? x[orderBy] - y[orderBy]
+            : y[orderBy] - x[orderBy]
+        )
     );
   };
 
@@ -112,7 +130,7 @@ const Explore = () => {
             entryFee = ethers.utils.formatUnits(entryFee.toString(), decimals);
 
             return {
-              id,
+              id: Number(id),
               status,
               startTime,
               endTime,
@@ -136,6 +154,20 @@ const Explore = () => {
   useEffect(() => {
     fetchPools();
   }, []);
+
+  useEffect(() => {
+    const [orderBy, orderDirection] = activeSort.split("-");
+    setOrderBy(orderBy);
+    setOrderDirection(orderDirection);
+
+    setFilteredPools(
+      [...filteredPools].sort((x, y) =>
+        orderDirection === "asc"
+          ? x[orderBy] - y[orderBy]
+          : y[orderBy] - x[orderBy]
+      )
+    );
+  }, [activeSort]);
 
   return (
     <Container>
@@ -185,95 +217,123 @@ const Explore = () => {
             <Pagination
               color="primary"
               count={loading ? 0 : Math.ceil(filteredPools.length / first)}
-              onChange={(e, page) => setSkip((page - 1) * first)}
+              onChange={(e, page) => {
+                setSkip((page - 1) * first);
+                setActivePage(page);
+              }}
+              page={activePage}
+              sx={{ paddingTop: 2 }}
             />
+            {!loading && (
+              <Typography sx={{ paddingTop: 2 }}>
+                Found {filteredPools.length} pools
+              </Typography>
+            )}
             <FormControl sx={{ width: 250 }}>
               <InputLabel id="sort">Sort</InputLabel>
               <Select
-                value={1}
+                value={activeSort}
                 label="Sort"
                 id="sort"
-                onChange={() => {}}
+                onChange={(e) => setActiveSort(e.target.value)}
                 size="small"
                 variant="filled"
               >
-                <MenuItem value={1}>ID (Asc)</MenuItem>
-                <MenuItem value={2}>ID (Desc)</MenuItem>
-                <MenuItem value={3}>Start Time (Asc)</MenuItem>
-                <MenuItem value={4}>Start Time (Desc)</MenuItem>
-                <MenuItem value={5}>Entry Count (Lowest First)</MenuItem>
-                <MenuItem value={6}>Entry Count (Highest First)</MenuItem>
+                <MenuItem value={"id-asc"}>ID (Lowest first)</MenuItem>
+                <MenuItem value={"id-desc"}>ID (Latest first)</MenuItem>
+                <MenuItem value={"entryCount-asc"}>
+                  Entry Count (Lowest First)
+                </MenuItem>
+                <MenuItem value={"entryCount-desc"}>
+                  Entry Count (Highest First)
+                </MenuItem>
               </Select>
             </FormControl>
           </Box>
           <Divider sx={{ marginTop: 3, marginBottom: 3 }} />
-          {loading ? (
-            <CircularProgress />
-          ) : (
-            <Grid container spacing={3}>
-              {filteredPools
-                .slice(skip, first + skip)
-                .map(
-                  ({
-                    id,
-                    status,
-                    startTime,
-                    endTime,
-                    entryCount,
-                    entryFee,
-                    token,
-                  }) => (
-                    <Grid item xs={12} md={4} key={id}>
-                      <Card>
-                        <CardHeader
-                          avatar={
-                            <Avatar sx={{ bgcolor: red[500] }} aria-label="id">
-                              {id}
-                            </Avatar>
-                          }
-                          // action={
-                          //   <IconButton aria-label="settings">
-                          //     <MoreVertIcon />
-                          //   </IconButton>
-                          // }
-                          title={`Status: ${status}`}
-                          subheader="Subheader here"
-                        />
-                        <CardContent>
-                          <Typography>
-                            Entry Fee: {entryFee} {token?.symbol}
-                          </Typography>
-                          <Typography>Entry Count: {entryCount}</Typography>
-                          {Date.now() < startTime && (
-                            <Typography>
-                              Entry Left: {maxEntryCount - entryCount}
-                            </Typography>
-                          )}
-                          <Typography>
-                            Pool deposit: {entryCount * entryFee}{" "}
-                            {token?.symbol}
-                          </Typography>
-                          {Date.now() > startTime ? (
-                            <Typography>
-                              Ends in: <Countdown date={endTime} />
-                            </Typography>
-                          ) : (
-                            <Typography>
-                              Starts in: <Countdown date={startTime} />
-                            </Typography>
-                          )}
-                        </CardContent>
-                        <CardActions disableSpacing>
-                          <Button onClick={() => navigate(`/pools/${id}`)}>
-                            View
-                          </Button>
-                        </CardActions>
-                      </Card>
-                    </Grid>
-                  )
-                )}
-            </Grid>
-          )}
+
+          <Grid container spacing={3}>
+            {loading
+              ? [...Array(first).keys()].map((index) => (
+                  <Grid item xs={12} md={6} key={index}>
+                    <Skeleton
+                      variant="rectangular"
+                      animation="wave"
+                      height={300}
+                    />
+                  </Grid>
+                ))
+              : filteredPools
+                  .slice(skip, first + skip)
+                  .map(
+                    ({
+                      id,
+                      status,
+                      startTime,
+                      endTime,
+                      entryCount,
+                      entryFee,
+                      token,
+                    }) => (
+                      <Grid item xs={12} md={6} key={id}>
+                        <Card>
+                          <CardHeader
+                            avatar={
+                              <Avatar
+                                sx={{
+                                  bgcolor: `${
+                                    status === "ACTIVE"
+                                      ? green[500]
+                                      : status === "CANCELLED"
+                                      ? red[500]
+                                      : blue[500]
+                                  }`,
+                                }}
+                                aria-label="id"
+                                variant="circular"
+                              >
+                                {id}
+                              </Avatar>
+                            }
+                            // action={
+                            //   <IconButton aria-label="settings">
+                            //     <MoreVertIcon />
+                            //   </IconButton>
+                            // }
+                            title={`Status: ${status}`}
+                            subheader="Subheader here"
+                          />
+                          <CardContent>
+                            <List>
+                              <ListItem>
+                                <FeeIcon /> &nbsp; Entry Fee: {entryFee}{" "}
+                                {token?.symbol}
+                              </ListItem>
+                              <ListItem>
+                                <CountIcon /> &nbsp; Entry Count: {entryCount}
+                              </ListItem>
+                              {status === "ACTIVE" && Date.now() < startTime && (
+                                <ListItem>
+                                  <LeftIcon /> &nbsp; Entry Left:{" "}
+                                  {maxEntryCount - entryCount}
+                                </ListItem>
+                              )}
+                              <ListItem>
+                                <DepositIcon /> &nbsp; Pool Deposit:{" "}
+                                {entryCount * entryFee} {token.symbol}
+                              </ListItem>
+                            </List>
+                          </CardContent>
+                          <CardActions disableSpacing>
+                            <Button onClick={() => navigate(`/pools/${id}`)}>
+                              View
+                            </Button>
+                          </CardActions>
+                        </Card>
+                      </Grid>
+                    )
+                  )}
+          </Grid>
         </Grid>
       </Grid>
     </Container>
